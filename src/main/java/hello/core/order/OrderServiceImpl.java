76b44023c738896b73e2019d94d4969ce5c0b0d2
jspark <sylvan0212@gmail.com>
2022-02-04 21:53:54 +0900

    다양한 의존관계 주입 방법

--- src/main/java/hello/core/order/OrderServiceImpl.java
+++ src/main/java/hello/core/order/OrderServiceImpl.java
@@ -12,7 +12,9 @@ public class OrderServiceImpl implements OrderService{
     private final MemberRepository memberRepository;
     private final DiscountPolicy discountPolicy;
 
-    @Autowired
+    // 생성자가 유일하다면 Autowired를 생략해도 된다.
+    // 하지만 2개 이상이면, 자동 주입 할 생성자 위에 Autowired를 넣어주어야 자동 주입이 된다.
+//    @Autowired
     public OrderServiceImpl(MemberRepository memberRepository, DiscountPolicy discountPolicy) {
         this.memberRepository = memberRepository;
         this.discountPolicy = discountPolicy;
