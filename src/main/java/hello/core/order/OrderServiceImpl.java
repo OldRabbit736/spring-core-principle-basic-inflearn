9d026bf69adb2939259e5f6a395d5207aac2118f
jspark <sylvan0212@gmail.com>
2022-02-05 19:11:08 +0900

    애노테이션 직접 만들기

--- src/main/java/hello/core/order/OrderServiceImpl.java
+++ src/main/java/hello/core/order/OrderServiceImpl.java
@@ -1,5 +1,6 @@
 package hello.core.order;
 
+import hello.core.annotation.MainDiscountPolicy;
 import hello.core.discount.DiscountPolicy;
 import hello.core.member.Member;
 import hello.core.member.MemberRepository;
@@ -18,7 +19,7 @@ public class OrderServiceImpl implements OrderService{
     // 생성자가 유일하다면 Autowired를 생략해도 된다.
     // 하지만 2개 이상이면, 자동 주입 할 생성자 위에 Autowired를 넣어주어야 자동 주입이 된다.
     @Autowired
-    public OrderServiceImpl(MemberRepository memberRepository, DiscountPolicy discountPolicy) {
+    public OrderServiceImpl(MemberRepository memberRepository, @MainDiscountPolicy DiscountPolicy discountPolicy) {
         this.memberRepository = memberRepository;
         this.discountPolicy = discountPolicy;
     }
