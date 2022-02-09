81d00fbe022f15740c0795f3c575400b4b3ce0c2
jspark <sylvan0212@gmail.com>
2022-02-09 20:18:56 +0900

    프로토타입 스코프 - 싱글톤 빈과 함께 사용시 Provider로 문제 해결

--- src/test/java/hello/core/scope/SingletonWithPrototypeTest1.java
+++ src/test/java/hello/core/scope/SingletonWithPrototypeTest1.java
@@ -2,12 +2,15 @@ package hello.core.scope;
 
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.ObjectFactory;
+import org.springframework.beans.factory.ObjectProvider;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 import org.springframework.context.annotation.Scope;
 
 import javax.annotation.PostConstruct;
 import javax.annotation.PreDestroy;
+import javax.inject.Provider;
 
 public class SingletonWithPrototypeTest1 {
 
@@ -24,28 +27,58 @@ public class SingletonWithPrototypeTest1 {
     }
 
     @Test
-    void singletonClientUsePrototype() {
-        AnnotationConfigApplicationContext ac = new AnnotationConfigApplicationContext(ClientBean.class, PrototypeBean.class);
+    void singletonClientUsePrototype1() {
+        AnnotationConfigApplicationContext ac = new AnnotationConfigApplicationContext(ClientBean1.class, PrototypeBean.class);
 
-        ClientBean clientBean1 = ac.getBean(ClientBean.class);
+        ClientBean1 clientBean1 = ac.getBean(ClientBean1.class);
         int count1 = clientBean1.logic();
         Assertions.assertThat(count1).isEqualTo(1);
 
-        ClientBean clientBean2 = ac.getBean(ClientBean.class);
+        ClientBean1 clientBean2 = ac.getBean(ClientBean1.class);
         int count2 = clientBean2.logic();
         Assertions.assertThat(count2).isEqualTo(2);
     }
 
+    @Test
+    void singletonClientUsePrototype2() {
+        AnnotationConfigApplicationContext ac = new AnnotationConfigApplicationContext(ClientBean2.class, PrototypeBean.class);
+
+        ClientBean2 clientBean1 = ac.getBean(ClientBean2.class);
+        int count1 = clientBean1.logic();
+        Assertions.assertThat(count1).isEqualTo(1);
+
+        ClientBean2 clientBean2 = ac.getBean(ClientBean2.class);
+        int count2 = clientBean2.logic();
+        Assertions.assertThat(count2).isEqualTo(1);
+    }
+
     @Scope("singleton")
-    static class ClientBean {
+    static class ClientBean1 {
         private final PrototypeBean prototypeBean;
 
         @Autowired
-        public ClientBean(PrototypeBean prototypeBean) {
+        public ClientBean1(PrototypeBean prototypeBean) {
             this.prototypeBean = prototypeBean;
         }
 
         public int logic() {
+            prototypeBean.addCount();
+            return prototypeBean.getCount();
+        }
+    }
+
+    @Scope("singleton")
+    static class ClientBean2 {
+
+        @Autowired
+//        private ObjectProvider<PrototypeBean> prototypeBeanProvider;    // spring
+//        private ObjectFactory<PrototypeBean> prototypeBeanProvider;   // spring
+        private Provider<PrototypeBean> prototypeBeanProviderStandard;    // java standard (JSR-330 Provider)
+
+        public int logic() {
+//            PrototypeBean prototypeBean = prototypeBeanProvider.getObject();
+            PrototypeBean prototypeBean = prototypeBeanProviderStandard.get();
+
             prototypeBean.addCount();
             return prototypeBean.getCount();
         }
